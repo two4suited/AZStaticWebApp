a812ef7c38b1c3416cda496ea363262a461452ce
Brian Sheridan <brian.sheridan@gmail.com>
2022-07-17 18:05:50 -0700

    Updated code

---
+++ Shared/Person.cs
@@ -0,0 +1,8 @@
+namespace BlazorApp.Api;
+
+public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string FullName => FirstName + " " + LastName;
+}
--- StaticWebAppFunction/GetPerson.cs
+++ StaticWebAppFunction/GetPerson.cs
@@ -1,33 +1,28 @@
-using System;
-using System.IO;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using BlazorApp.Api;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
-namespace StaticWebAppFunction;
-
-public static class GetPerson
+namespace StaticWebAppFunction
 {
-    [FunctionName("GetPerson")]
-    public static async Task<IActionResult> RunAsync(
-        [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, ILogger log)
+    public static class GetPeople
     {
-        log.LogInformation("C# HTTP trigger function processed a request.");
+        [FunctionName("GetPeople")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            var people = new List<Person>
+            {
+                new Person { FirstName = "Brian", LastName = "Sheridan" },
+                new Person { FirstName = "Heidi", LastName = "Sheridan" }
+            };
 
-        string name = req.Query["name"];
-
-        string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-        dynamic data = JsonConvert.DeserializeObject(requestBody);
-        name = name ?? data?.name;
-
-        return name != null
-            ? (ActionResult) new OkObjectResult($"Hello, {name}")
-            : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
-        
+            return new OkObjectResult(people);
+        }
     }
 }
